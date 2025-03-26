e5d910452d9937cf6cd098d98494a27ac9a290fe
ASG40K <anthonygalindo185@gmail.com>
2025-03-26 15:02:04 -0700

    Add project files.

---
+++ newprojectapp.client/src/app/fetch-data/fetch-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fetch-data',
+  standalone: false,
+  templateUrl: './fetch-data.component.html',
+  styleUrl: './fetch-data.component.css'
+})
+export class FetchDataComponent {
+
+}
